12147bada6b062aeabbc3a85d4ffa8d348d81fdb
Harrison Fung <harrison.hok.fung@gmail.com>
2022-07-07 18:49:56 +0000

    Completed Time and Driver in folder 5

---
+++ programming/5/time-driver/Driver.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * Driver class for Time class  by Team LucidThinkeren
+ * First Last
+ * collaborators: First Last, First Last
+ */
+
+/**
+   INSTRUCTIONS:
+   This file contains the Driver starter code for our Time class.
+   
+   Add code in main to test all your methods
+   
+   Place this file in a folder named programming/5/Driver.java
+*/
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+	//declare a var of appropriate type to assign an instance of Time to
+	Time t;
+
+	//assign var the address of a newly-apportioned Time object
+	t = new Time(5,30,30);
+  Time h;
+  h = new Time(0,2,0);
+  t.set(5,28,30);
+  t.add(h);
+  System.out.println(t.toString());
+  int compare = t.compareTo(h);
+  System.out.println(compare);
+    }//end main()
+
+}//end class
