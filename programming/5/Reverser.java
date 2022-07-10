fe92f895987217840e0419b8dc2142ea7ef8a190
Harrison Fung <harrison.hok.fung@gmail.com>
2022-07-10 21:54:12 +0000

    Finished Reverser.java in folder 5

--- programming/5/Reverser.java
+++ programming/5/Reverser.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.*;
 
 /**
  * Recursive String Reverser by Team Palindromotosis
@@ -13,20 +14,48 @@ public class Reverser
      String reverseF(String) -- recursive String reverser
      precond:  input String is non-empty
      postcond: returns input String with chars in reverse order
-  */
-  public static String reverseR( String s )
+  //  Second method using a for loop
+      String dummyString = "";
+      for (int i = s.length() - 1; i >= 0; i--){
+        dummyString = dummyString + s.charAt(i);
+      }
+    return dummyString;
+**/
+  public static String reverseR( String s, String firstString)
   {
-    /* YOUR SIMPLE, SMART IMPLEMENTATION HERE */
-  }
+    /** using recursion but added an additional parameter for holding the final value because we can't reintialiaze over and over and need a way to keep incrementing the final value
+      **/
+    // create an end variable to get the length of the string
+    int end = s.length();
 
+    // create a way to get the final character after each iteration
+    String dummyString = "" + s.charAt(end - 1);
+
+    // increment the final value by the letter 
+    firstString += dummyString;
+
+    // base case
+    if (end == 1){
+    return firstString;
+    } 
+    // otherwise decrement end and take the substring from a different value
+    else {
+      end -= 1;
+      return reverseR(s.substring(0, end), firstString); 
+    }
+  }
 
   public static void main( String[] args )
   {
-    System.out.println( reverseR("odd") );
-    System.out.println( reverseR("even") );
-    System.out.println( reverseR("abcdefg") );
-    System.out.println( reverseR("stressed") );
-    System.out.println( reverseR("amanaplanacanalPanama") );
+    System.out.println( reverseR("odd", "") ); 
+    System.out.println();
+    System.out.println( reverseR("even", "") );
+    System.out.println();
+    System.out.println( reverseR("abcdefg", "") );
+    System.out.println();
+    System.out.println( reverseR("stressed", "") );
+    System.out.println();
+    System.out.println( reverseR("amanaplanacanalPanama", "") );
 
     //more tests welcome
     // got some good palindromes?
